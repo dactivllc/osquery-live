c60514e75ff47c205e3ddffd04a711d67426808d
Zachary Wasserman <zachwass2000@gmail.com>
2019-01-30 21:22:51 -0800

    Remove unsafe origin check

--- server/server.go
+++ server/server.go
@@ -15,9 +15,6 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
 }
 
 func shellHandler(w http.ResponseWriter, r *http.Request) {
