589460cfea6beffaa066144dcecc70b529a88fb3
Zachary Wasserman <zachwass2000@gmail.com>
2019-02-01 14:40:35 -0800

    Improve documentation

--- server/websockets.go
+++ server/websockets.go
@@ -5,10 +5,13 @@ import (
 	"github.com/pkg/errors"
 )
 
+// websocketWrapper wraps a websocket.Conn to implement the io.Reader and
+// io.Writer interfaces. This makes it easy to connect to stdio of a pty.
 type websocketWrapper struct {
 	conn *websocket.Conn
 }
 
+// Read implements the io.Reader interface.
 func (w *websocketWrapper) Read(p []byte) (int, error) {
 	_, reader, err := w.conn.NextReader()
 	if err != nil {
@@ -23,6 +26,7 @@ func (w *websocketWrapper) Read(p []byte) (int, error) {
 	return n, nil
 }
 
+// Write implements the io.Writer interface.
 func (w *websocketWrapper) Write(p []byte) (int, error) {
 	writer, err := w.conn.NextWriter(websocket.TextMessage)
 	if err != nil {
